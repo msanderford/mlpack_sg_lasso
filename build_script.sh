27fdabce982f944a3e7296b603ce0bada8fdc2b9
Maxwell Sanderford <tuf79348@temple.edu>
2021-03-31 22:40:40 -0400

    Specify compiler for cmake.

--- build_script.sh
+++ build_script.sh
@@ -7,7 +7,7 @@ cp CMakeLists.txt tempfile.txt
 head -n 20 tempfile.txt > CMakeLists.txt
 echo add_compile_definitions(_GLIBCXX_USE_CXX11_ABI=0) >> CMakeLists.txt
 tail -n +21 tempfile.txt >> CMakeLists.txt
-cmake ../
+CC="gcc" cmake ../
 make mlpack_sg_lasso mlpack_sg_lasso_leastr mlpack_overlapping_sg_lasso_leastr
 cd ../../ol_sg_lasso_preprocessing_src
 g++ -std=c++11 -o preprocess preprocess_main.cpp preprocess.cpp
