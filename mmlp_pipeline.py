1c2b40dbad70d5ba0480e717b21545f9b78eb5f2
msanderford <66372831+msanderford@users.noreply.github.com>
2021-06-14 10:57:49 -0700

    Added ensemble training options.

--- mmlp_pipeline.py
+++ mmlp_pipeline.py
@@ -5,14 +5,34 @@ import pipeline_funcs as pf
 
 def main(args):
 	hypothesis_file_list = pf.generate_hypothesis_set(args.tree, args.nodelist, args.response)
-	features_filename_list, groups_filename_list, response_filename_list, gene_list = pf.generate_input_matrices(args.aln_list, hypothesis_file_list, args)
-	weights_file_list = pf.run_mlp(features_filename_list, groups_filename_list, response_filename_list, args.lambda1, args.lambda2)
-	pf.process_weights(weights_file_list, hypothesis_file_list, groups_filename_list, features_filename_list, gene_list)
-	for hypothesis_filename in hypothesis_file_list:
-		shutil.move(hypothesis_filename, args.output)
-		shutil.move(hypothesis_filename.replace(".txt","_out_feature_weights.xml"), args.output)
-		shutil.move(hypothesis_filename.replace("hypothesis.txt","gene_predictions.txt"), args.output)
-		shutil.move(hypothesis_filename.replace("hypothesis.txt", "mapped_feature_weights.txt"), args.output)
+	if args.ensemble is not None and args.ensemble >= 1:
+		tempdir_list = []
+		for i in range(0, args.ensemble_coverage):
+			partitioned_aln_lists = pf.split_gene_list(args.aln_list)
+			for part_aln_list in partitioned_aln_lists:
+				tempdir = "{}_rep{}_part{}".format(args.output, i, j)
+				tempdir_list.append(tempdir)
+				features_filename_list, groups_filename_list, response_filename_list, gene_list = pf.generate_input_matrices(part_aln_list, hypothesis_file_list, args)
+				weights_file_list = pf.run_mlp(features_filename_list, groups_filename_list, response_filename_list, args.lambda1, args.lambda2)
+				pf.process_weights(weights_file_list, hypothesis_file_list, groups_filename_list, features_filename_list, gene_list)
+				for hypothesis_filename in hypothesis_file_list:
+					shutil.move(hypothesis_filename, args.output)
+					shutil.move(hypothesis_filename.replace(".txt","_out_feature_weights.xml"), args.output)
+					shutil.move(hypothesis_filename.replace("hypothesis.txt","gene_predictions.txt"), args.output)
+					shutil.move(hypothesis_filename.replace("hypothesis.txt", "mapped_feature_weights.txt"), args.output)
+				shutil.move(args.output, tempdir)
+		os.mkdir(args.output)
+		for tempdir in tempdir_list:
+			shutil.move(tempdir, args.output)
+	else:
+		features_filename_list, groups_filename_list, response_filename_list, gene_list = pf.generate_input_matrices(args.aln_list, hypothesis_file_list, args)
+		weights_file_list = pf.run_mlp(features_filename_list, groups_filename_list, response_filename_list, args.lambda1, args.lambda2)
+		pf.process_weights(weights_file_list, hypothesis_file_list, groups_filename_list, features_filename_list, gene_list)
+		for hypothesis_filename in hypothesis_file_list:
+			shutil.move(hypothesis_filename, args.output)
+			shutil.move(hypothesis_filename.replace(".txt","_out_feature_weights.xml"), args.output)
+			shutil.move(hypothesis_filename.replace("hypothesis.txt","gene_predictions.txt"), args.output)
+			shutil.move(hypothesis_filename.replace("hypothesis.txt", "mapped_feature_weights.txt"), args.output)
 
 
 if __name__ == '__main__':
@@ -26,6 +46,8 @@ if __name__ == '__main__':
 	parser.add_argument("-o", "--output", help="Output directory.", type=str, default="output")
 	parser.add_argument("--upsample_balance", help="Balance positive and negative response sets by upsampling the underpopulated set.", action='store_true', default=False)
 	parser.add_argument("--downsample_balance", help="Balance positive and negative response sets by downsampling the overpopulated set.", action='store_true', default=False)
+	parser.add_argument("--ensemble", help="Build gene-wise ensemble models, splitting the set of genes into N partitions for each run.", type=int, default=None)
+	parser.add_argument("--ensemble_coverage", help="Number of ensemble models to build. Each gene will be included in this many individual models.", type=int, default=5)
 	args = parser.parse_args()
 	main(args)
 	# generate_gene_prediction_table(weights_filename, responses_filename, groups_filename, features_filename, output_filename)
