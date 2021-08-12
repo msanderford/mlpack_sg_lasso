5e1f549b650c37bb62b1c2057cad1dc723ee3352
msanderford <66372831+msanderford@users.noreply.github.com>
2021-08-11 17:03:40 -0700

    Make build_script.sh cluster-compatible.

--- build_script.sh
+++ build_script.sh
@@ -2,9 +2,14 @@ wget https://www.mlpack.org/files/mlpack-3.2.2.tar.gz
 tar -xvzpf mlpack-3.2.2.tar.gz
 cp -R sg_lasso_src/src mlpack-3.2.2
 cd mlpack-3.2.2
+cp CMakeLists.txt tempfile.txt
+head -n 20 tempfile.txt > CMakeLists.txt
+echo 'add_compile_definitions(_GLIBCXX_USE_CXX11_ABI=0)' >> CMakeLists.txt
+tail -n +21 tempfile.txt >> CMakeLists.txt
+rm tempfile.txt
 mkdir build
 cd build
-cmake ../
+CC="gcc" cmake ../
 make mlpack_sg_lasso mlpack_sg_lasso_leastr mlpack_overlapping_sg_lasso_leastr
 cd ../../ol_sg_lasso_preprocessing_src
 g++ -std=c++11 -o preprocess preprocess_main.cpp preprocess.cpp
