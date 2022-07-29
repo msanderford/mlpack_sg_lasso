9508436386ffb03ab180198ab62583cce08d1cd8
msanderford <66372831+msanderford@users.noreply.github.com>
2022-07-29 11:40:28 -0700

    Update preprocess_main.cpp

--- src/preprocess_main.cpp
+++ src/preprocess_main.cpp
@@ -4,6 +4,19 @@
 
 using namespace std;
 
+void handler(int sig) {
+	void *array[10];
+	size_t size;
+
+	// get void*'s for all entries on the stack
+	size = backtrace(array, 10);
+
+	// print out all the frames to stderr
+	fprintf(stderr, "Error: signal %d:\n", sig);
+	backtrace_symbols_fd(array, size, STDERR_FILENO);
+	exit(1);
+}
+
 bool pathExists(const std::string &s)
 {
 	struct stat buffer;
@@ -12,6 +25,7 @@ bool pathExists(const std::string &s)
 
 int main(int argc, char *argv[])
 {
+	signal(SIGSEGV, handler);
 	cout << "You have entered " << argc - 1 << " arguments:" << "\n";
 
 	if (argc < 4)
