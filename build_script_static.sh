80bd561845f67675a31c8bafab06b0455b3fe01e
msanderford <66372831+msanderford@users.noreply.github.com>
2022-12-20 17:43:06 -0800

    Added overlapping logistic sglasso

--- build_script_static.sh
+++ build_script_static.sh
@@ -5,7 +5,8 @@ g++-8 -std=c++17 preprocess_main.cpp preprocess.cpp -o preprocess -static
 g++-8 -std=c++17 sg_lasso_main.cpp sg_lasso.cpp /usr/lib/x86_64-linux-gnu/libopenblas.a /usr/lib/x86_64-linux-gnu/liblapack.a /usr/lib/x86_64-linux-gnu/libpthread.a -o sg_lasso -Iinclude -static
 g++-8 -std=c++17 sg_lasso_leastr_main.cpp sg_lasso_leastr.cpp /usr/lib/x86_64-linux-gnu/libopenblas.a /usr/lib/x86_64-linux-gnu/liblapack.a /usr/lib/x86_64-linux-gnu/libpthread.a -o sg_lasso_leastr -Iinclude -static
 g++-8 -std=c++17 overlapping_sg_lasso_leastr_main.cpp overlapping_sg_lasso_leastr.cpp /usr/lib/x86_64-linux-gnu/libopenblas.a /usr/lib/x86_64-linux-gnu/liblapack.a /usr/lib/x86_64-linux-gnu/libpthread.a -o overlapping_sg_lasso_leastr -Iinclude -static
-cp preprocess sg_lasso sg_lasso_leastr overlapping_sg_lasso_leastr ../bin
+g++-8 -std=c++17 overlapping_sg_lasso_logisticr_main.cpp overlapping_sg_lasso_logisticr.cpp /usr/lib/x86_64-linux-gnu/libopenblas.a /usr/lib/x86_64-linux-gnu/liblapack.a /usr/lib/x86_64-linux-gnu/libpthread.a -o overlapping_sg_lasso_logisticr -Iinclude -static
+cp preprocess sg_lasso sg_lasso_leastr overlapping_sg_lasso_leastr overlapping_sg_lasso_logisticr ../bin
 cd ..
 tar -czvf bin.tar.gz bin/
 cp bin.tar.gz ../ESL_master
